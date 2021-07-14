7b027ec10eaeedd9427bbb533b17612400ce55f2
roach <roach@cleaninglab.co.kr>
2021-07-15 03:54:19 +0900

    setup: alim worker

---
+++ alim-worker/src/main/java/com/roachdev/alimworker/AlimWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.roachdev.alimworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlimWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlimWorkerApplication.class, args);
+	}
+
+}
