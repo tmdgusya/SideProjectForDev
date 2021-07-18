6e970b3a1039c78ca60057a3b65798e8bdb3d8b8
roach <roach@cleaninglab.co.kr>
2021-07-19 00:11:16 +0900

    [#15] TypeORM DB Configuration

--- api-server/src/app.module.ts
+++ api-server/src/app.module.ts
@@ -1,7 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const defaultDBOptions: TypeOrmModule = {
+  type: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: '1234',
+  database: 'study',
+  entities: [],
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...defaultDBOptions,
+      name: 'master',
+      port: 3307,
+    }),
+    TypeOrmModule.forRoot({
+      ...defaultDBOptions,
+      name: 'test',
+      port: 3307,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
