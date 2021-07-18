841ec54ab9f2a36a1d412efadef32db4f6b4233b
roach <roach@cleaninglab.co.kr>
2021-07-19 01:13:10 +0900

    [#18] Kotlin Annotation Processing 을 위한 kapt 추가

--- api-server/build.gradle.kts
+++ api-server/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 	id("org.springframework.boot") version "2.4.8"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.32"
+	kotlin("kapt") version "1.4.32"
 	kotlin("plugin.spring") version "1.4.32"
 	kotlin("plugin.jpa") version "1.4.32"
 }
@@ -17,6 +18,8 @@ repositories {
 }
 
 dependencies {
+	api("com.querydsl:querydsl-jpa:4.2.2")
+	kapt("com.querydsl:querydsl-apt:4.2.2:jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-redis-reactive")
 	implementation("org.springframework.boot:spring-boot-starter-web")
