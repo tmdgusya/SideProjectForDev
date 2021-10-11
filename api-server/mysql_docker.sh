12182f4cad7464d445496223e6759591697fbbf2
roach <tmdgusya@naver.com>
2021-10-11 12:09:23 +0900

    feat: mysql dockerfile 추가

---
+++ api-server/mysql_docker.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+docker run -d --rm --name moa-mysql \
+  -e MYSQL_ROOT_PASSWORD=1234 \
+  -e MYSQL_ROOT_HOST='%' \
+  -e MYSQL_DATABASE=study \
+  -e TZ=Asia/Seoul \
+  -p 3307:3306 mysql:5.7 --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci --sql_mode=""
